3d86a6c6e60f2fb70a1c26cfab8b4ed0d17a56dc
LLY <719194963@qq.com>
2024-04-11 12:16:03 +0800

    debug

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -23,8 +23,8 @@ public interface EmployeeMapper {
      * @param employee
      * @return
      */
-    @Insert("insert into emploee (name,username,password,phone,sex,id_number,create_time,update_time,create_user,updata_user)" +
+    @Insert("insert into employee (name,username,password,phone,sex,id_number,status,create_time,update_time,create_user,update_user) " +
             "values " +
-            "#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createtime},#{updatetime},#{createuser},#{updatauser}")
+            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
 }
